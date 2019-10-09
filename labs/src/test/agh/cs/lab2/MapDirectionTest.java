24017d81b3b2dea2c4f086d7dfab3b19908992ed
Jck-eN <jaceknit.rac@gmail.com>
2019-10-09 20:53:54 +0200

    Competed 2. lab

--- labs/src/test/agh/cs/lab2/MapDirectionTest.java
+++ labs/src/test/agh/cs/lab2/MapDirectionTest.java
@@ -1,22 +1,22 @@
 package agh.cs.lab2;
 
 import agh.cs.lab2.MapDirection;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MapDirectionTest {
     @Test
     public void nextTest(){
-        Assert.assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
-        Assert.assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
-        Assert.assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
-        Assert.assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
     }
     @Test
     public void previousTest(){
-        Assert.assertEquals(MapDirection.WEST.previous(), MapDirection.SOUTH);
-        Assert.assertEquals(MapDirection.NORTH.previous(), MapDirection.WEST);
-        Assert.assertEquals(MapDirection.EAST.previous(), MapDirection.NORTH);
-        Assert.assertEquals(MapDirection.SOUTH.previous(), MapDirection.EAST);
+        assertEquals(MapDirection.WEST.previous(), MapDirection.SOUTH);
+        assertEquals(MapDirection.NORTH.previous(), MapDirection.WEST);
+        assertEquals(MapDirection.EAST.previous(), MapDirection.NORTH);
+        assertEquals(MapDirection.SOUTH.previous(), MapDirection.EAST);
     }
 }
--- labs/src/test/agh/cs/lab2/Vector2dTest.java
+++ labs/src/test/agh/cs/lab2/Vector2dTest.java
@@ -1,16 +1,71 @@
 package agh.cs.lab2;
 
-import agh.cs.lab2.MapDirection;
-import agh.cs.lab2.Vector2d;
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class Vector2dTest {
+    Vector2d v1 = new Vector2d(3, 4);
+    Vector2d v2 = new Vector2d(0, -1);
+    Vector2d v3 = new Vector2d(0, -1);
+    Vector2d v4 = new Vector2d(6, 2);
+    MapDirection dir = MapDirection.EAST;
     @Test
     public void equalsTest(){
-        Assert.assertEquals(MapDirection.NORTH , MapDirection.SOUTH.next());
-        Assert.assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
-        Assert.assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
-        Assert.assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        assertTrue(v2.equals(v3));
+        assertFalse(v1.equals(v2));
+        assertFalse(v1.equals(dir));
+    }
+
+    @Test
+    public void toStringTest(){
+        assertEquals("(3,4)", v1.toString() );
+        assertEquals("(0,-1)", v2.toString() );
+    }
+
+    @Test
+    public void precedesTest(){
+        assertTrue(v2.precedes(v1));
+        assertTrue(v2.precedes(v3));
+        assertFalse(v1.precedes(v3));
+        assertFalse(v1.precedes(v4));
+    }
+    @Test
+    public void followsTest(){
+        assertTrue(v1.follows(v2));
+        assertTrue(v3.follows(v2));
+        assertFalse(v3.follows(v1));
+        assertFalse(v4.follows(v1));
+    }
+
+    @Test
+    public void upperRightTest(){
+        assertEquals(v1.upperRight(v2), new Vector2d(3,4));
+        assertEquals(v2.upperRight(v3), new Vector2d(0,-1));
+        assertEquals(v4.upperRight(v1), new Vector2d(6,4));
+    }
+    @Test
+    public void lowerLeftTest(){
+        assertEquals(v1.lowerLeft(v2), new Vector2d(0,-1));
+        assertEquals(v2.lowerLeft(v3), new Vector2d(0,-1));
+        assertEquals(v4.lowerLeft(v1), new Vector2d(3,2));
+    }
+    @Test
+    public void addTest(){
+        assertEquals(v1.add(v2), new Vector2d(3,3));
+        assertEquals(v2.add(v3), new Vector2d(0,-2));
+        assertEquals(v4.add(v1), new Vector2d(9,6));
+    }
+    @Test
+    public void subtractTest(){
+        assertEquals(v1.subtract(v2), new Vector2d(3,5));
+        assertEquals(v2.subtract(v3), new Vector2d(0,0));
+        assertEquals(v4.subtract(v1), new Vector2d(3,-2));
+    }
+    @Test
+    public void oppositeTest(){
+        assertEquals(v1.opposite(), new Vector2d(-3,-4));
+        assertEquals(v2.opposite(), new Vector2d(0,1));
+        assertEquals(v4.opposite(), new Vector2d(-6,-2));
     }
 }
